43589ff56fdc7d1b1965168d5e8f7950f78eb3fa
luelldolopes <40871222+luelldolopes@users.noreply.github.com>
2019-03-24 20:11:02 -0300

    Add files via upload

---
+++ provas/arquivo_1.php
@@ -0,0 +1,362 @@
+<script id="rendered-js"> (function () { const myQuestions = [{ question: "1 - De acordo com o Código de Trânsito Brasileiro, uma linha amarela pintada junto a calçada significa:",
+    answers: {
+      a: "permitido estacionar",  
+      b: "proibido estacionar",
+      c: " o estacionamento é oblíquo",
+      d: "o estacionamento é paralelo" },
+
+    correctAnswer: "b" },
+{ question: "2 - Qual a velocidade que um ônibus pode desenvolver em uma rodovia sem sinalização?",
+    answers: {
+      a: "70kmh",  
+      b: "80kmh",
+      c: "90kmh",
+      d: "100kmh" },
+
+    correctAnswer: "c" },
+{ question: "3 - De acordo com a sinalização horizontal, uma linha divisória de faixa de trânsito pintada na cor Branca:",
+    answers: {
+      a: "Separa fluxos de trânsito de vários sentidos",  
+      b: "Separa fluxos de trânsito de sentidos duplo",
+      c: "Separa fluxos de trânsito de sentidos a esquerda",
+      d: "Separa fluxos de trânsito de mesmo sentido" },
+
+    correctAnswer: "d" },
+{ question: "4 - Qual é a ordem ideal dos procedimentos, no local dos acidentes?",
+    answers: {
+      a: "primeiro atender as vítimas depois sinalizar o local",  
+      b: "primeiro atender as vítimas depois sinalizar o local e após chamar o resgate",
+      c: "primeiro atender as vítimas depois sinalizar o local, e depois se evadir do local",
+      d: "primeiro sinalizar o local, acionar o socorro e socorrer as vítimas" },
+
+    correctAnswer: "d" },
+{ question: "5 - Conforme o Código de trânsito, como se classificam as vias rurais:",
+    answers: {
+      a: "Arterial e Rápida e Rodovias",  
+      b: "Vias de Trânsito Rápido, Arteriais, Coletoras e Locais",
+      c: "Rodovias e Estradas",
+      d: "Vicinais Expressas e Preferenciais" },
+
+    correctAnswer: "c" },
+{ question: "6 - É comum ocorrerem acidentes de trânsito com vitimas. Diante dessa situação qual é a nossa obrigação legal em relação a prestação de socorro as vitimas de acidentes?",
+    answers: {
+      a: "é obrigação de todos prestarem socorro às vitimas",  
+      b: "é obrigação de todos prestarem socorro somente aos pedestres",
+      c: "é obrigação de todos prestarem socorro às vitimas, desde que não haja riscos para quem socorre",
+      d: "A obrigação é somente dos órgãos de Trânsito" },
+
+    correctAnswer: "c" },
+{ question: "7 - É comum existir aglomeração (filas) de veículos em frente as escolas devido à imprudência dos motoristas (pais dos alunos) que param os seus veículos em fila dupla para o embarque e desembarque dos alunos. Esta ação causa risco e atraso no trânsito. Esta atitude deve ser?",
+    answers: {
+      a: "imitada, pois todos fazem assim",  
+      b: "ironizada, pois eles não sabem o que fazem",
+      c: "evitada por ser uma infração de trânsito",
+      d: "considerada normal" },
+
+    correctAnswer: "c" },
+{ question: "8 - Quando no trânsito existe uma placa de regulamentação permitindo o estacionamento, você entende que é permitido estacionar:",
+    answers: {
+      a: "Dentro do espaço permitido em até cinqüenta centímetros distante do passeio",  
+      b: "A dez metros da placa",
+      c: "A mais de oito metros do alinhamento da via transversal",
+      d: "Antes da placa" },
+
+    correctAnswer: "a" },
+{ question: "9 - São documentos exigidos para fazer a transferência do veículo:",
+    answers: {
+      a: "CRV, RG e comprovante de residência",  
+      b: "CRV e CNH",
+      c: "Seguro obrigatório e DUT",
+      d: "Carteira de Habilitação e autorização do proprietário do veiculo" },
+
+    correctAnswer: "a" },
+{ question: "10 - A suspensão do direito de dirigir ocorre quando:",
+    answers: {
+      a: "Quando cometer qualquer infração gravíssima",  
+      b: "Quando atingir 20 pontos por infração em doze meses",
+      c: "Somente quando beber ao dirigir",
+      d: "Quando a carteira for cassada" },
+
+    correctAnswer: "b" },
+{ question: "11 - Dirigir defensivamente significa evitar acidentes apesar das situações adversas. Quando ocorre um acidente as estatísticas mostram que a maior parte da culpa recai sobre:",
+    answers: {
+      a: "o veículo",  
+      b: "o condutor",
+      c: "situações adversas",
+      d: "as vias" },
+
+    correctAnswer: "b" },
+{ question: "12 - Quando uma vitima apresentar fraturas expostas com sangramentos deve-se:",
+    answers: {
+      a: "estancar a hemorragia e imobilizar a parte fraturada com algo rígido enfaixando junto ao membro",  
+      b: "imobilizar a parte fraturada, depois estancar a hemorragia",
+      c: "colocar gelo na fratura",
+      d: "colocar o osso quebrado no lugar imediatamente" },
+
+    correctAnswer: "a" },
+{ question: "13 - Bons exemplos devem ser seguidos; no trânsito quem deve dar bons exemplos?",
+    answers: {
+      a: "os motoristas",  
+      b: "os pedestres",
+      c: "os ciclistas",
+      d: "todos os usuários das vias" },
+
+    correctAnswer: "d" },
+{ question: "14 - As estatísticas mostram que uma das principais causas ou principal causa de acidentes é o uso de bebidas alcoólicas, ao dirigir de acordo com a nova lei que regulamenta este assunto, se o condutor fizer uso de álcool ao dirigir está sujeito a:",
+    answers: {
+      a: "Multa, Suspensão do direito de dirigir, recolhimento da CNH e sujeito prisão do condutor",  
+      b: "Multa e Retenção do veiculo apenas",
+      c: "Multa",
+      d: "Multa e Prisão do Condutor apenas" },
+
+    correctAnswer: "a" },
+{ question: "15 - Quando o Proprietário do veículo empresta seu carro a alguém e, este condutor comete uma infração de trânsito. Qual é o prazo mínimo que o proprietário tem para informar ao Detran quem estava na direção do veículo?",
+    answers: {
+      a: "cinco dias",  
+      b: "10dias",
+      c: "15 dias",
+      d: "30 dias" },
+
+    correctAnswer: "c" },
+{ question: "16 - Após chegar ao local de um acidente a policia constatou que um veículo havia se partido ao meio e também teria derrubado um poste da rede elétrica. No local havia uma placa R-19 regulamentando a velocidade máxima em quarenta km por hora, nesse caso pode se concluir que a causa do acidente foi?",
+    answers: {
+      a: "imprudência e atenção",  
+      b: "imprudência e o excesso de velocidade",
+      c: "negligência e imperícia",
+      d: "andar em velocidade abaixo da permitida" },
+
+    correctAnswer: "b" },
+{ question: "17 - Quando o condutor desobedece a uma placa de regulamentação no trânsito, estará cometendo?",
+    answers: {
+      a: "Multa",  
+      b: "Infração de trânsito",
+      c: "Notificação",
+      d: "Penalidade" },
+
+    correctAnswer: "b" },
+{ question: "18 - Em relação ao novo modelo de Carteira de Habilitação,",
+    answers: {
+      a: "Tem validade de acordo com o exame de sanidade física e mental.",  
+      b: "Pode o motorista portar em copia autenticada.",
+      c: "Deve ser renovada a cada dez anos.",
+      d: "Deve ser renovada a cada ano." },
+
+    correctAnswer: "a" },
+{ question: "19 - Após a aquisição de um veiculo usado, qual é o prazo para efetuar a transferência do referido veículo?",
+    answers: {
+      a: "até o fim do ano vigente",  
+      b: "um mês",
+      c: "trinta dias",
+      d: "um ano" },
+
+    correctAnswer: "c" },
+{ question: "20 - Se um veículo ao transitar nas vias públicas produzir ruídos excedentes, devido a instalação de silenciadores inadequados, estará sujeito a:",
+    answers: {
+      a: "multa",  
+      b: "multa e remoção do veiculo",
+      c: "multa e retenção do veiculo",
+      d: "multa e suspensão do direto de dirigir" },
+
+    correctAnswer: "c" },
+{ question: "21 - Ao atender uma vitima de acidente com parada cardiorespiratória, qual a seqüência correta das ações?",
+    answers: {
+      a: "Fazer duas respirações seguidas de trinta massagens cardíacas, por cinco sessões completas",  
+      b: "Fazer duas respirações seguidas de quinze massagens cardíacas, por quatro sessões completas",
+      c: "Fazer uma respiração seguida de trinta massagens cardíacas, por cinco sessões completas",
+      d: "Fazer cinco respirações seguidas de dez massagens cardíacas, por dez sessões completas" },
+
+    correctAnswer: "a" },
+{ question: "22 - Qual é o programa instituído pelo CONAMA que regulamenta os níveis de poluição produzida por veículos automotores?",
+    answers: {
+      a: "Projeto JARI",  
+      b: "Eco 92",
+      c: "PROCONVE",
+      d: "Carta da Terra" },
+
+    correctAnswer: "c" },
+{ question: "23 - Qual a categoria da CNH utilizada para dirigir veículos de passageiros com capacidade acima de oito lugares sem contar o condutor?",
+    answers: {
+      a: "A",  
+      b: "B",
+      c: "C",
+      d: "D" },
+
+    correctAnswer: "d" },
+{ question: "24 - Quais as principais fontes de emissão de poluentes nas grandes cidades?",
+    answers: {
+      a: "Extração do Petróleo",  
+      b: "Queima de combustíveis fósseis",
+      c: "Os veículos e as indústrias",
+      d: "Mineração" },
+
+    correctAnswer: "c" },
+{ question: "25 - Quando alguém deseja interromper a via pública depende de:",
+    answers: {
+      a: "Autorização do Prefeito Municipal",  
+      b: "Autorização do DETRAN",
+      c: "Autorização do DER",
+      d: "Prévia autorização da autoridade de trânsito" },
+
+    correctAnswer: "d" },
+{ question: "26 - Qual a idade que uma pessoa deve ter para se habilitar nas categorias (D) e (E)?",
+    answers: {
+      a: "vinte e um anos",  
+      b: "dezessete anos",
+      c: "dezoito anos",
+      d: "dezenove anos" },
+
+    correctAnswer: "a" },
+{ question: "27 - Qual é a posição correta para estacionar uma motocicleta em relação a calçada?",
+    answers: {
+      a: "perpendicular ao alinhamento da calçada",  
+      b: "paralelo a guia da calçada",
+      c: "a mais de 50 cm da guia da calçada",
+      d: "de oblíquo" },
+
+    correctAnswer: "a" },
+{ question: "28 - Até quantos anos de idade uma criança deve andar no banco traseiro?",
+    answers: {
+      a: "cinco anos",  
+      b: "dez anos",
+      c: "seis anos",
+      d: "sete anos" },
+
+    correctAnswer: "b" },
+{ question: "29 - Uma placa de Sinalização de trânsito com o formato circular e cores vermelha, branca e preta são de:",
+    answers: {
+      a: "Regulamentação",  
+      b: "Advertência",
+      c: "Indicação",
+      d: "Educativa" },
+
+    correctAnswer: "a" },
+{ question: "30 - Como todos podem ajudar a evitar novos acidentes no local que já existe acidente trânsito?",
+    answers: {
+      a: "Sinalizando corretamente o local, colocando o triangulo a uma distancia de no mínimo trinta metros antes do local do local",  
+      b: "ceder sempre seu direito para os outros",
+      c: "fazer aulas de direção para reforçar os reflexos no trânsito",
+      d: "deixar para que as autoridades decidam o que fazer" },
+
+    correctAnswer: "a" },
+];
+function buildQuiz() {
+    // armazenar saida HTML
+    const output = [];
+
+    // para cada questao...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // armazenar as alternativas
+      const answers = [];
+
+      // para cada alternativa...
+      for (letter in currentQuestion.answers) {
+        // ...adicionar radio
+        answers.push(
+        `<label>
+             <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+           </label>`);
+
+      }
+
+      // adicionar questao e alternativas na saida
+      output.push(
+      `<div class="slide">
+           <div class="question"> ${currentQuestion.question} </div>
+           <div class="answers"> ${answers.join("")} </div>
+         </div>`);
+
+    });
+
+    // combinar lista de saida e mostrar na pagina
+    quizContainer.innerHTML = output.join("");
+  }
+
+  function showResults() {
+    // combinar alternativas
+    const answerContainers = quizContainer.querySelectorAll(".answers");
+
+    // respostas do usuario
+    let numCorrect = 0;
+
+    // para cada questao...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      // alternativa correta
+      if (userAnswer === currentQuestion.correctAnswer) {
+        // add to the number of correct answers
+        numCorrect++;
+
+        // alternativa verde
+        answerContainers[questionNumber].style.color = "lightgreen";
+      } else {
+        // alternativa errada ou em branco
+        // alternativa vermelha
+        answerContainers[questionNumber].style.color = "red";
+      }
+    });
+
+    // mostrar numero de acertos
+    resultsContainer.innerHTML = `Acertou ${numCorrect} de ${myQuestions.length}`;
+  }
+
+  function showSlide(n) {
+    slides[currentSlide].classList.remove("active-slide");
+    slides[n].classList.add("active-slide");
+    currentSlide = n;
+
+    if (currentSlide === 0) {
+      previousButton.style.display = "none";
+    } else {
+      previousButton.style.display = "inline-block";
+    }
+
+    if (currentSlide === slides.length - 1) {
+      nextButton.style.display = "none";
+      submitButton.style.display = "inline-block";
+    } else {
+      nextButton.style.display = "inline-block";
+      submitButton.style.display = "inline-block";
+    }
+  }
+
+  function showNextSlide() {
+    showSlide(currentSlide + 1);
+  }
+
+  function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+  }
+
+    function restartQuiz() {
+    parent.window.document.location.href = 'quiz.php';
+  }
+
+  const quizContainer = document.getElementById("quiz");
+  const resultsContainer = document.getElementById("results");
+  const submitButton = document.getElementById("submit");
+
+  // exibir questoes
+  buildQuiz();
+
+  const restartButton = document.getElementById("restart");
+  const previousButton = document.getElementById("previous");
+  const nextButton = document.getElementById("next");
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+
+  showSlide(0);
+
+  // enviar, mostrar resultado
+  restartButton.addEventListener("click", restartQuiz);
+  submitButton.addEventListener("click", showResults);
+  previousButton.addEventListener("click", showPreviousSlide);
+  nextButton.addEventListener("click", showNextSlide);
+})();
+      //# sourceURL=pen.js
+    </script>
